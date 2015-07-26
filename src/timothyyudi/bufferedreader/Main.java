448f1e8163e9cb9f7de914a8f13943134ec19287
Timothy Yudi <timothyyudi@gmail.com>
2015-07-26 15:47:06 +0700

    add 3 and 4

--- src/timothyyudi/bufferedreader/Main.java
+++ src/timothyyudi/bufferedreader/Main.java
@@ -13,10 +13,10 @@ public class Main {
 	static String txtURI;
 	static int intBuf=0;
 	static char cBuf;
-	static char[] cBuf2;
+	static char[] cBuf2, cBuf3, cBuf4;
 	static String sBuf;
 	static long algoStart, algoEnd, timeFragment, timeTotal = 0;
-	static String output_Home, output_processTimeRead1, output_processTimeRead2;
+	static String output_Home, output_processTimeRead1, output_processTimeRead2, output_processTimeRead3, output_processTimeRead4;
 	
 	public static void main(String[] args) { // arg[0] = num of characters to be processed at once. arg[1] = file name. 
 //		txtURI = args[0];
@@ -24,6 +24,8 @@ public class Main {
 		output_Home = "C:\\Java AhoCorasick\\BufferedReader results\\";
 		output_processTimeRead1 = output_Home+"BufferedReader_Results 1.txt";
 		output_processTimeRead2 = output_Home+"BufferedReader_Results 2.txt";
+		output_processTimeRead1 = output_Home+"BufferedReader_Results 3.txt";
+		output_processTimeRead2 = output_Home+"BufferedReader_Results 4.txt";
 		
 		switch (args[0]) {
 		case "1":
@@ -84,6 +86,66 @@ public class Main {
 			}
 			break;
 		
+		case "3":
+			cBuf3 = new char[3];
+			
+			algoStart = System.nanoTime();
+			try {
+				File file = new File(txtURI);
+				FileReader fileReader = new FileReader(file);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+				bufferedReader.readLine();
+				while (bufferedReader.read(cBuf3, 0, 3) != -1) {
+					sBuf = String.copyValueOf(cBuf3);
+//					System.out.println(sBuf);
+				}
+				fileReader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			algoEnd = System.nanoTime();
+			timeTotal = algoEnd-algoStart;
+			
+			PrintWriter processTimerWriter3;
+			try {
+				processTimerWriter3 = new PrintWriter(new BufferedWriter(new FileWriter(output_processTimeRead3, true)));
+				processTimerWriter3.println(""+timeTotal);
+			    processTimerWriter3.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			break;
+			
+		case "4":
+			cBuf4 = new char[4];
+			
+			algoStart = System.nanoTime();
+			try {
+				File file = new File(txtURI);
+				FileReader fileReader = new FileReader(file);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+				bufferedReader.readLine();
+				while (bufferedReader.read(cBuf4, 0, 4) != -1) {
+					sBuf = String.copyValueOf(cBuf4);
+//					System.out.println(sBuf);
+				}
+				fileReader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			algoEnd = System.nanoTime();
+			timeTotal = algoEnd-algoStart;
+			
+			PrintWriter processTimerWriter4;
+			try {
+				processTimerWriter4 = new PrintWriter(new BufferedWriter(new FileWriter(output_processTimeRead4, true)));
+				processTimerWriter4.println(""+timeTotal);
+			    processTimerWriter4.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			break;
+			
 		default:
 			break;
 		}
